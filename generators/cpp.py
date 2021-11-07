572bab682dc5e146c70c98d0a0796d4e9aa2b33c
Erich Schroeter <erich.schroeter@gmail.com>
2021-11-07 03:16:22 -0600

    Added Qualifiers and tests

--- generators/cpp.py
+++ generators/cpp.py
@@ -1,3 +1,74 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Qualifier:
+    """Represents a C++ qualifier keyword, and follows the decorator pattern to allow chaining."""
+    
+    decorator: Optional['Qualifier']
+    keyword: str
+    
+    def __call__(self):
+        return f"{self.keyword} {self.decorator.keyword}" if self.decorator else f"{self.keyword}"
+
+
+class Static(Qualifier):
+    
+    def __init__(self, decorator=None):
+        super().__init__(keyword='static', decorator=decorator)
+
+
+class Inline(Qualifier):
+    
+    def __init__(self, decorator=None):
+        super().__init__(keyword='inline', decorator=decorator)
+
+
+class Volatile(Qualifier):
+    
+    def __init__(self, decorator=None):
+        super().__init__(keyword='volatile', decorator=decorator)
+
+
+class Virtual(Qualifier):
+    
+    def __init__(self, decorator=None):
+        super().__init__(keyword='virtual', decorator=decorator)
+
+
+class Const(Qualifier):
+    
+    def __init__(self, decorator=None):
+        super().__init__(keyword='const', decorator=decorator)
+
+
+class Constexpr(Qualifier):
+    
+    def __init__(self, decorator=None):
+        super().__init__(keyword='constexpr', decorator=decorator)
+
+
+class Extern(Qualifier):
+    
+    def __init__(self, decorator=None):
+        super().__init__(keyword='extern', decorator=decorator)
+
+
+def is_const(qualifier: Qualifier) -> bool:
+    if isinstance(qualifier, Const):
+        return True
+    if qualifier and qualifier.decorator:
+        return is_const(qualifier.decorator)
+    return False
+
+
+def is_constexpr(qualifier: Qualifier) -> bool:
+    if isinstance(qualifier, Constexpr):
+        return True
+    if qualifier and qualifier.decorator:
+        return is_constexpr(qualifier.decorator)
+    return False
+
 
 # TODO move CppDeclaration here
 # TODO move CppImplementation here
--- test/cpp/test_variable.py
+++ test/cpp/test_variable.py
@@ -2,6 +2,52 @@ import io
 import unittest
 from code_generator import CppFile
 from cpp_generator import CppVariable
+from generators.cpp import Static, Inline, Volatile, Const, Constexpr, Extern, Virtual, is_const, is_constexpr
+
+
+class TestCppQualifiers(unittest.TestCase):
+
+    def test_static(self):
+        self.assertEqual('static', Static()())
+
+    def test_inline(self):
+        self.assertEqual('inline', Inline()())
+
+    def test_volatile(self):
+        self.assertEqual('volatile', Volatile()())
+
+    def test_virtual(self):
+        self.assertEqual('virtual', Virtual()())
+
+    def test_const(self):
+        self.assertEqual('const', Const()())
+
+    def test_constexpr(self):
+        self.assertEqual('constexpr', Constexpr()())
+
+    def test_extern(self):
+        self.assertEqual('extern', Extern()())
+
+    def test_static_const(self):
+        self.assertEqual('static const', Static(Const())())
+
+
+class TestIsConst(unittest.TestCase):
+
+    def test_const_only(self):
+        self.assertTrue(is_const(Const()))
+
+    def test_static_const(self):
+        self.assertTrue(is_const(Static(Const())))
+
+
+class TestIsConstexpr(unittest.TestCase):
+
+    def test_constexpr_only(self):
+        self.assertTrue(is_constexpr(Constexpr()))
+
+    def test_static_constexpr(self):
+        self.assertTrue(is_constexpr(Static(Constexpr())))
 
 
 class TestCppVariableGenerator(unittest.TestCase):
