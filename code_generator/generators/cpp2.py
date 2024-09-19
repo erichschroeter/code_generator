d6c944ae9fd5baa265fdc7d43a5ae99ffd9d4f69
Erich Schroeter <erich.schroeter@gmail.com>
2024-09-18 20:27:35 -0500

    feat: Variable class

---
+++ code_generator/generators/cpp2.py
@@ -0,0 +1,42 @@
+
+import re
+
+
+CPP_IDENTIFIER_REGEX = r'^[a-zA-Z_]+[a-zA-Z0-9_]*$'
+CPP_IDENTIFIER_PATTERN = re.compile(CPP_IDENTIFIER_REGEX)
+
+
+class CppSyntaxError(Exception):
+    pass
+
+
+class CppIdentifierError(CppSyntaxError):
+    def __init__(self, identifier) -> None:
+        self.identifier = identifier
+        self.message = f'Invalid C++ identifier: {identifier}'
+        super().__init__()
+
+
+class CppTypeError(CppSyntaxError):
+    def __init__(self, type) -> None:
+        self.type = type
+        self.message = f'Invalid C++ type: {type}'
+        super().__init__()
+
+
+class Variable:
+    def __init__(self, name, type='void', qualifiers=None) -> None:
+        if not CPP_IDENTIFIER_PATTERN.fullmatch(name):
+            raise CppIdentifierError(name)
+        if not CPP_IDENTIFIER_PATTERN.fullmatch(type):
+            raise CppTypeError(type)
+        self.name = name
+        self.type = type
+        self.qualifiers = qualifiers
+
+    def __str__(self) -> str:
+        return self.name
+
+    def declaration_str(self):
+        qualifiers = ' '.join(self.qualifiers) + ' ' if self.qualifiers is not None else ''
+        return f'{qualifiers}{self.type} {self.name}'
---
+++ code_generator/test/cpp/test_variable_cpp2.py
@@ -0,0 +1,58 @@
+from textwrap import dedent
+import unittest
+
+from code_generator.generators.cpp2 import CppIdentifierError, CppTypeError, Variable
+
+
+class TestVariable(unittest.TestCase):
+
+    def test_raises_CppIdentifierError_starts_with_digit(self):
+        self.assertRaises(CppIdentifierError, Variable, '0')
+
+    def test_raises_CppIdentifierError_starts_with_digit(self):
+        self.assertRaises(CppIdentifierError, Variable, '0')
+
+    def test_name_x(self):
+        self.assertTrue(Variable('x'))
+
+    def test_name_X(self):
+        self.assertTrue(Variable, 'X')
+
+    def test_name_aA(self):
+        self.assertTrue(Variable, 'aA')
+
+    def test_name_aAunderscore(self):
+        self.assertTrue(Variable, 'aA_')
+
+    def test_name_aAunderscore0(self):
+        self.assertTrue(Variable, 'aA_0')
+
+    def test_name_aA0underscore(self):
+        self.assertTrue(Variable, 'aA0_')
+
+    def test_name_Aa(self):
+        self.assertTrue(Variable, 'Aa')
+
+    def test_name_underscore(self):
+        self.assertTrue(Variable, '_')
+
+    def test_name_underscore0(self):
+        self.assertTrue(Variable, '_0')
+
+    def test_decl_with_default_type(self):
+        self.assertEqual(Variable('x').declaration_str(), 'void x')
+
+    def test_decl_with_custom_type(self):
+        self.assertEqual(Variable('x', type='bool').declaration_str(), 'bool x')
+
+    def test_decl_raises_CppTypeError_with_type_with_whitespace(self):
+        self.assertRaises(CppTypeError, Variable, 'x', type='int whitespace')
+
+    def test_decl_with_custom_one_qualifier(self):
+        self.assertEqual(Variable('x', qualifiers=['extern']).declaration_str(), 'extern void x')
+
+    def test_decl_with_custom_two_qualifier(self):
+        self.assertEqual(Variable('x', qualifiers=['extern', 'const']).declaration_str(), 'extern const void x')
+
+    def test_str(self):
+        self.assertEqual(str(Variable('x')), 'x')
